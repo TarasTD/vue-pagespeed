4aa3a3b3d42699b2a908cae723542ee80df0be07
tarastd <tarasdmytrus@gmail.com>
2018-02-02 18:42:02 +0200

    initial commit

---
+++ src/views/main-view.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    HELLO WORLD
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainView',
+  data () {
+    return {
+      msg: 'MAIN'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
